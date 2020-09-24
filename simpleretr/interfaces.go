c2025d4ea273b0f28d71da5e2788a30d81ae0a46
Mike Goelzer <mike@protocol.ai>
2020-09-24 20:25:44 +0000

    Crashes on start - experimenting

--- simpleretr/interfaces.go
+++ simpleretr/interfaces.go
@@ -3,7 +3,7 @@ package simpleretr
 import (
 	//"context"
 
-	inet "github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/network"
 	//"github.com/libp2p/go-libp2p-core/peer"
 )
 
@@ -17,18 +17,5 @@ type Server interface {
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
-	HandleStream(stream inet.Stream)
-}
-
-// Client is the requesting side of the ChainExchange protocol. It acts as
-// a proxy for other components to request chain data from peers. It is chiefly
-// used by the Syncer.
-type Client interface {
-
-	// TODO:  delete this
-	//	// GetBlocks fetches block headers from the network, from the provided
-	//	// tipset *backwards*, returning as many tipsets as the count parameter,
-	//	// or less.
-	//	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-
+	HandleStream(stream network.Stream)
 }
